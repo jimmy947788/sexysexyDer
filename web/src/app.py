b9490bcfb1c7bf85aa6f1fb71dd23d9424e09471
吉掰米 Jimmy <jimmy.wu@daedalus.com.tw>
2021-06-05 16:33:53 +0800

    更新前端顯示待發送數量

--- web/src/app.py
+++ web/src/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, make_response, render_template, request
 import logging
 import os
 import pika
+import time
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 QUEUE_NAME="sexsexder"
@@ -12,33 +12,39 @@ QUEUE_NAME="sexsexder"
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    msg1 = ""
-    msg2  = ""
-    post_linker = ""
     if request.method == 'POST':
         if request.form.get('txt_ig_linker'): 
             post_linker = request.form.get('txt_ig_linker')
-
-            connection = pika.BlockingConnection(pika.ConnectionParameters(os.getenv('MQ_HOST')))
-            channel = connection.channel()
-            channel.queue_declare(queue=QUEUE_NAME)
             channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=post_linker)
 
-    return render_template('index.html', message=msg1, post_linker=post_linker, error_message=msg2)
+            queue = channel.queue_declare(queue=QUEUE_NAME) #, durable=True,exclusive=False, auto_delete=False)
+            print(queue.method.message_count)
+
+    return render_template('index.html', message="")
+
+@app.route('/GetWaitMessageCount', methods=['GET'])
+def getWaitMessageCount():
+    queue = channel.queue_declare(queue=QUEUE_NAME)
+    count = str(queue.method.message_count)
+    response = make_response(count, 200)
+    response.mimetype = "text/plain"
+    return response
 
 if __name__ == "__main__":
 
     if not os.path.exists("logs"):
         os.makedirs("logs")
     logging.basicConfig(filename='./logs/sexsexder.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-    
-    #credentials = pika.PlainCredentials('guest', 'guest')
-    #parameters = pika.ConnectionParameters('rabbitmq', 5672,'/',credentials)
-    """
-    connection = pika.BlockingConnection(pika.ConnectionParameters("192.168.0.105"))
+
+    global channel 
+    print("sleep 10s waiting container discover rabbitmq")
+    time.sleep(10)
+
+    load_dotenv() 
+    mqHost = os.getenv("MQ_HOST")
+    print(f"mqHost={mqHost }")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(mqHost))
     channel = connection.channel()
-    channel.queue_declare(queue=QUEUE_NAME)
-    """
 
     app.config['TEMPLATES_AUTO_RELOAD'] = True      
     app.jinja_env.auto_reload = True
